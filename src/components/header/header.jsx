00d46b2e934d14c5550e5f2ebb4e748d5ad19546
abhik7872 <chatterjeeabhik90@gmail.com>
2022-05-07 23:06:47 +0530

    Worked on header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import CTA from './CTA'
+import Social from './socials'
+import ME from '../../assets/avatar_1.webp'
 import './header.css'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Abhik Chatterjee</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <Social />
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+      </div>
+    </header>
   )
 }
 
