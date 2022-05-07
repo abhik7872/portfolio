534fe47e9492af267a1fb772539e1aedac957f42
abhik7872 <chatterjeeabhik90@gmail.com>
2022-05-07 20:06:20 +0530

    Worked on css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
     <div>
       <>
         <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Contact />
+        <Footer />
       </>
     </div>
   )
