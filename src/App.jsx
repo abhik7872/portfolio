74e2faf9bf99cb7f938b065cb4d2009ad118d890
abhik7872 <chatterjeeabhik90@gmail.com>
2022-05-07 18:46:46 +0530

    Started from scratch with react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/header'
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Header />
+      </>
+    </div>
+  )
+}
+
+export default App
